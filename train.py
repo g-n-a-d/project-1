022ca8b83ffdd5ccfb0c90bd782f8bc64cc45611
Dang Nguyen <117255808+g-n-a-d@users.noreply.github.com>
2023-02-13 16:30:54 +0700

    Update train.py

--- train.py
+++ train.py
@@ -1,17 +1,16 @@
 import torch
-from yaml import dump
+import matplotlib.pyplot as plt
 
-def train(model, lossFunction, dataloader_train, dataloader_val=None, num_epochs=50, lr=1e-5, milestones_lr=[36], gamma=0.1, device=torch.device('cpu'), display=False, log=False):
+def train(model, lossFunction, dataloader_train, dataloader_val=None, num_epochs=50, lr=1e-5, milestones_lr=[36], gamma=0.1, device=torch.device('cpu'), display=False):
     criterion = lossFunction
     optimizer = torch.optim.Adam(params=model.parameters(), lr=lr)
     scheduler = torch.optim.lr_scheduler.MultiStepLR(optimizer, milestones=milestones_lr, gamma=gamma)
     model.train()
     for epoch in range(num_epochs):
-        log_loss = []
-        for i, (r, l) in enumerate(dataloader_train):
-            r = r.to(device)
+        for i, (x, l) in enumerate(dataloader_train):
+            x = x.to(device)
             l = l.to(device)
-            pred = model(r)
+            pred = model(x)
             loss = criterion(pred, l)
             optimizer.zero_grad()
             loss.backward()
@@ -22,14 +21,14 @@ def train(model, lossFunction, dataloader_train, dataloader_val=None, num_epochs
                     num_epochs,
                     i + 1,
                     len(dataloader_train),
-                    loss.item()))
-               if dataloader_dev != None:
-                    cm = confusionMatrix(model, dataloader_val, device)
-                    print('confusion matrix:')
-                    print(cm)
-                    print('accuracy: {}'.format((torch.trace(cm)/cm.sum()).item()))
-            log_loss.append(loss.item())
+                    loss.item()
+                ))
+        if display and dataloader_val != None:
+            cm = confusionMatrix(model, pred.shape[1], dataloader_val, device)
+            plt.matshow(cm)
+            for ii in range(cm.shape[0]):
+                for iii in range(cm.shape[0]):
+                    plt.text(iii, ii, str(cm[ii, iii].item()), va='center', ha='center')
+            plt.show()
+            print('accuracy: {}'.format((torch.trace(cm)/cm.sum()).item()))
         scheduler.step()
-        if log:
-            with open('log_loss_{}.yaml'.format(type(model).__name__), 'a') as f:
-                dump({'epoch_{}'.format(epoch + 1) : log_loss}, f, default_flow_style=False)
