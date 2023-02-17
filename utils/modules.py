adea55f9b82e14545935af4ff7cf89d2bf419a38
Dang Nguyen <117255808+g-n-a-d@users.noreply.github.com>
2023-02-17 12:06:46 +0700

    Update modules.py

--- utils/modules.py
+++ utils/modules.py
@@ -12,3 +12,28 @@ class downLayer(torch.nn.Module):
     def forward(self, x):
         out = self.down(x)
         return out
+    
+class v2iDownLayer(torch.nn.Module):
+    def __init__(self, inputDim, outputShape=(224, 224), numLayer=3):
+        super().__init__()
+        layerList = torch.nn.ModuleList([torch.nn.Sequential(
+            torch.nn.Conv1d(1, inputDim//2, kernel_size=7, padding=3),
+            torch.nn.ReLU(),
+            torch.nn.AvgPool1d(kernel_size=2)
+        )])
+        for i in range(1, numLayer):
+            layerList.append(torch.nn.Sequential(
+                torch.nn.Conv1d(inputDim//(2**i), inputDim//(2**(i + 1)), kernel_size=7, padding=3),
+                torch.nn.ReLU(),
+                torch.nn.AvgPool1d(kernel_size=2)
+            ))
+            # layerList.append(torch.nn.Conv1d(inputDim//(2**i), inputDim//(2**(i + 1)), kernel_size=7, padding=3))
+        self.down = torch.nn.Sequential(*layerList)
+        self.sampler = torch.nn.Upsample(size=outputShape)
+
+    def forward(self, x):
+        out = x.unsqueeze(dim=1)
+        out = self.down(out)
+        out = out.unsqueeze(dim=1)
+        out = self.sampler(out)
+        return out
