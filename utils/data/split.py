191cd73c42c95ea26043c7c7a8bb186ca1b488a5
Dang Nguyen <117255808+g-n-a-d@users.noreply.github.com>
2023-02-19 22:42:24 +0700

    Update split.py

--- utils/data/split.py
+++ utils/data/split.py
@@ -11,3 +11,25 @@ def APISeq_split_TFIDF(df, test_size=0.12):
     tf_idf = TF_IDF(words)
     x_train, x_test, y_train, y_test = train_test_split(words, label, test_size=test_size)
     return x_train, x_test, y_train, y_test, tf_idf
+
+def APISeq_split_token(df, test_size=0.1):
+    words = list(map(lambda s: s.split(','), list(df['api'])))
+    token = {}
+    counter = 1
+    for wordi in words:
+        for wordii in wordi:
+            if wordii not in token.keys():
+                token[wordii] = counter
+                counter += 1
+    for i in range(len(words)):
+        for ii in range(len(words[i])):
+            words[i][ii] = token[words[i][ii]]
+    label = list(df['class'])
+    x_train, x_test, y_train, y_test = train_test_split(words, label, test_size=test_size)
+    return x_train, x_test, y_train, y_test, counter
+
+def APISeq_split_DB(df, test_size=0.1):
+    words = list(map(lambda s: s.split(','), list(df['api'])))
+    label = list(df['class'])
+    x_train, x_test, y_train, y_test = train_test_split(words, label, test_size=test_size)
+    return x_train, x_test, y_train, y_test
