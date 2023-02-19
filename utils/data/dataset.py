ca570cfca0067faa588388164f9dbcec95e95788
Dang Nguyen <117255808+g-n-a-d@users.noreply.github.com>
2023-02-19 22:40:48 +0700

    Update dataset.py

--- utils/data/dataset.py
+++ utils/data/dataset.py
@@ -21,7 +21,6 @@ class BODMAS(torch.utils.data.Dataset):
 
 class APISeq_TFIDF(torch.utils.data.Dataset):
     def __init__(self, x, label, tfidf, size=768):
-        super().__init__()
         self.x = x
         self.label = list(map(lambda w: [w], label))
         self.tfidf = tfidf
@@ -41,5 +40,25 @@ class APISeq_TFIDF(torch.utils.data.Dataset):
             x_ = x_[:self.size]
         elif x_.shape[0] < self.size:
             x_ = torch.cat((x_, torch.zeros(self.size - x_.shape[0])), dim=0)
+        label_ = torch.Tensor(self.ohe.transform([self.label[index]]).toarray().reshape(-1,))
+        return x_, label_
+
+class APISeq_token(torch.utils.data.Dataset):
+    def __init__(self, x, label, size=256):
+        self.x = x
+        self.label = list(map(lambda w: [w], label))
+        self.size = size
+        self.ohe = OneHotEncoder(handle_unknown='ignore')
+        self.ohe.fit(self.label)
+
+    def __len__(self):
+        return len(self.x)
+
+    def __getitem__(self, index):
+        x_ = torch.Tensor(self.x[index]).int()
+        if x_.shape[0] > self.size:
+            x_ = x_[:self.size]
+        elif x_.shape[0] < self.size:
+            x_ = torch.cat((x_, torch.zeros(self.size - x_.shape[0]).int()), dim=0)
         label_ = torch.Tensor(self.ohe.transform([self.label[index]]).toarray().reshape(-1,))
         return x_, label_
