a49ff4fe5ce08c8c85c648af48addc92b4be36c2
Dang Nguyen <117255808+g-n-a-d@users.noreply.github.com>
2023-02-19 11:52:59 +0700

    Update modules.py

--- utils/modules.py
+++ utils/modules.py
@@ -17,15 +17,13 @@ class v2iDownLayer(torch.nn.Module):
     def __init__(self, inputDim, numLayer=3):
         super().__init__()
         layerList = torch.nn.ModuleList([torch.nn.Sequential(
-            torch.nn.Conv1d(1, inputDim//2, kernel_size=7, padding=3),
-            torch.nn.ReLU(),
-            torch.nn.AvgPool1d(kernel_size=2)
+            torch.nn.Conv1d(1, inputDim//2, kernel_size=7, stride=2, padding=3),
+            torch.nn.ReLU()
         )])
         for i in range(1, numLayer):
             layerList.append(torch.nn.Sequential(
-                torch.nn.Conv1d(inputDim//(2**i), inputDim//(2**(i + 1)), kernel_size=3, padding=1),
-                torch.nn.ReLU(),
-                torch.nn.AvgPool1d(kernel_size=2)
+                torch.nn.Conv1d(inputDim//(2**i), inputDim//(2**(i + 1)), kernel_size=7, stride=2, padding=3),
+                torch.nn.ReLU()
             ))
         self.down = torch.nn.Sequential(*layerList)
 
