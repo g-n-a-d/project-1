d9f686534cad974f5f987655697488c41a3c9e00
Dang Nguyen <117255808+g-n-a-d@users.noreply.github.com>
2023-02-19 14:51:53 +0700

    Update data.py

--- utils/data.py
+++ utils/data.py
@@ -4,9 +4,9 @@ from sklearn.preprocessing import OneHotEncoder
 
 class BODMAS(torch.utils.data.Dataset):
     def __init__(self, x, label):
-        self.label = label
+        self.label = list(map(lambda w: [w], label))
         self.ohe = OneHotEncoder(handle_unknown='ignore')
-        self.ohe.fit(label)
+        self.ohe.fit(self.label)
         self.scaler = MinMaxScaler()
         self.scaler.fit(x)
         self.x = self.scaler.transform(x)
@@ -16,5 +16,30 @@ class BODMAS(torch.utils.data.Dataset):
 
     def __getitem__(self, index):
         x_ = torch.Tensor(self.x[index])
-        label_ = torch.Tensor(self.ohe.transform([[self.label[index]]]).toarray().reshape(-1,))
+        label_ = torch.Tensor(self.ohe.transform([self.label[index]]).toarray().reshape(-1,))
+        return x_, label_
+
+class APISeq_TFIDF(torch.utils.data.Dataset):
+    def __init__(self, x, label, tfidf, size=768):
+        super().__init__()
+        self.x = x
+        self.label = list(map(lambda w: [w], label))
+        self.tfidf = tfidf
+        self.size = size
+        self.ohe = OneHotEncoder(handle_unknown='ignore')
+        self.ohe.fit(self.label)
+
+    def __len__(self):
+        return len(self.x)
+
+    def __getitem__(self, index):
+        x_ = []
+        for token in self.x[index]:
+            x_.append(self.tfidf[token])
+        x_ = torch.Tensor(x_)
+        if x_.shape[0] > self.size:
+            x_ = x_[:self.size]
+        elif x_.shape[0] < self.size:
+            x_ = torch.cat((x_, torch.zeros(self.size - x_.shape[0])), dim=0)
+        label_ = torch.Tensor(self.ohe.transform([self.label[index]]).toarray().reshape(-1,))
         return x_, label_
